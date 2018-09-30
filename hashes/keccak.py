363cb4b8f3669f83eb4396df9a44819303b9dfeb
Daniel Cronce <daniel.cronce@wildcardcorp.com>
2018-09-30 03:54:19 -0500

    Keccak512 seems to be working.

--- hashes/keccak.py
+++ hashes/keccak.py
@@ -1,5 +1,7 @@
 from math import log
 from samson.utilities.bytes import Bytes
+from samson.utilities.manipulation import left_rotate
+from samson.constructions.sponge_construction import SpongeConstruction
 
 # https://github.com/ctz/keccak/blob/master/keccak.py
 
@@ -23,44 +25,68 @@ R = [
 
 # https://keccak.team/keccak_specs_summary.html
 class Keccak(object):
-    def __init__(self, r, c):
+    def __init__(self, r, c, digest_size):
         self.r = r
         self.c = c
         self.w = (r + c) // 25
 
-        self.n = log(self.w, 2) * 2 + 12
+        self.n = int(log(self.w, 2) * 2 + 12)
+        self.sponge = SpongeConstruction(self.keccak_f, self.pad, r, c)
+        self.digest_size = (digest_size // 8)
+
+
+    def pad(self, in_bytes):
+        bit_rate_bytes = (self.r + 7) // 8
+        pad_len = bit_rate_bytes - len(in_bytes)
+
+        if pad_len == 0:
+            pad_len = bit_rate_bytes
+
+        if pad_len == 1:
+            return in_bytes + bytes([0x81])
+        else:
+            return in_bytes + bytes([0x01] + ([0] * (pad_len - 2)) + [0x80])
 
 
     def keccak_f(self, A):
         for i in range(self.n):
+            # print(i)
             A = self.round_func(A, RC[i])
+            # print(A)
         return A
 
 
     def round_func(self, A, rc):
-        C = Bytes(b'\x00' * 4)
-        for x in range(4):
+        # print(A)
+        C = [0] * 5
+        for x in range(5):
             C[x] = A[x][0] ^ A[x][1] ^ A[x][2] ^ A[x][3] ^ A[x][4]
 
-        D = Bytes(b'\x00' * 4)
-        for x in range(4):
-            D[x] = C[x-1] ^ C[x+1].rrot(1)
+        D = [0] * 5
+        for x in range(5):
+            D[x] = C[x-1] ^ left_rotate(C[(x+1) % 5], 1, 64)
 
 
-        for x in range(4):
-            for y in range(4):
+        for x in range(5):
+            for y in range(5):
                 A[x][y] = A[x][y] ^ D[x]
 
-        B = Bytes(b'\x00' * 16)
-        for x in range(4):
-            for y in range(4):
-                B[y][2*x + 3*y] = A[x][y].rrot(R[x][y])
+
+        B = [[0] * 5 for _ in range(5)]
+        for x in range(5):
+            for y in range(5):
+                B[y][(2*x + 3*y) % 5] = left_rotate(A[x][y], R[y][x], 64)
 
 
-        for x in range(4):
-            for y in range(4):
-                A[x][y] = B[x][y] ^ ((~B[x+1, y]) & B[x+2, y])
+        for x in range(5):
+            for y in range(5):
+                A[x][y] = B[x][y] ^ ((~B[(x+1) % 5][y]) & B[(x+2) % 5][y])
 
         A[0][0] ^= rc
 
         return A
+    
+
+    def hash(self, plaintext):
+        self.sponge.absorb(Bytes.wrap(plaintext))
+        return sum(self.sponge.squeeze(self.digest_size))[:self.digest_size]
